659a40d50b54272c30cac7f980d52ca7ace75e95
antthegreekgod <hector.sarrion@gmail.com>
2024-11-21 09:50:57 +0100

    Adding Directory and Contents for FTP server, setting rate limit, and max amount of connections

--- honeypot/ftp/ftp.py
+++ honeypot/ftp/ftp.py
@@ -1,18 +1,41 @@
+import logging
+
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 
-
+# Instantiate a dummy authorizer for managing 'virtual' users
 authorizer = DummyAuthorizer()
 
-# add admin user
-authorizer.add_user("admin", "admin", ".", perm="elradfmw")
+# Define user admin:admin having full r/w permissions
+authorizer.add_user("admin", "admin", "./ftp_directory", perm="lr")
 
-# add anon user
-authorizer.add_anonymous(".", perm="elradfmw")
+# add anon user, he will only be able to list the files in the directory, but won't have enough privileges to interact :)
+authorizer.add_anonymous("./ftp_directory", perm="l")
 
+# Instantiate FTP handler class
 handler = FTPHandler
 handler.authorizer = authorizer
 
-server = FTPServer(("127.0.0.1", 21), handler)
+
+# Define a customized banner (string returned when client connects)
+# I'm going to setup a banner to emulate a vulnerable version of ProFTPd vulnerable to RCE
+handler.banner =  "ProFTPD 1.3.5 Server (Debian)"
+
+
+
+# Instantiate FTP server class and listen on all interfaces, port 2121
+address = ('', 21)
+server = FTPServer(address, handler)
+
+
+# set a limit for connections
+# for now it can handle 10 simultaneous connections and it has been rate limited to 3 connections from a unique ip
+server.max_cons = 10
+server.max_cons_per_ip = 3
+
+# create a file on pwd to log everything
+logging.basicConfig(filename='./myftp.log', level=logging.INFO)
+
+# start ftp server
 server.serve_forever()
